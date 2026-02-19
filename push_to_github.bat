3bf488e8e75a1c7ddace1644ca3cd0f190262efe
sanjayy-22 <sanjai462004@gmail.com>
2026-02-19 13:45:12 +0530

    Deploy to GitHub Pages

--- push_to_github.bat
+++ push_to_github.bat
@@ -7,21 +7,30 @@ if not exist .git (
     echo Git already initialized.
 )
 
+echo Setting branch to main...
+git branch -M main
+
 echo Configuring Remote...
 git remote remove origin 2>nul
-git remote add origin https://github.com/tnsanjai29/B2Gserver.git
-echo Remote origin set to https://github.com/tnsanjai29/B2Gserver.git
+git remote add origin https://github.com/Dhanush-creates/b2gai.git
+echo Remote origin set to https://github.com/Dhanush-creates/b2gai.git
+
+echo Building client...
+cd client
+call npm install
+call npm run build
+cd ..
+
+echo Moving built files to root...
+move client\dist\* . >nul 2>&1
 
 echo Adding files...
 git add .
 
 echo Committing...
-git commit -m "Initial commit of GOV-AID project"
+git commit -m "Deploy to GitHub Pages"
 
 echo Pushing to GitHub...
-git push -u origin master
-if errorlevel 1 (
-    git push -u origin main
-)
-echo Done.
+git push -u origin main
+echo Done. GitHub Pages URL: https://dhanush-creates.github.io/b2gai/
 pause
